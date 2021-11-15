d3bc768b8395d7b93130b4000d2f8c001c4966a0
Brent Mode <bmode8@gmail.com>
2021-11-14 23:02:56 -0600

    Finished a correct version of the autocorrect feature. Merging into main.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use fractal::{parse_complex, parse_pair, pixel_to_point, render_julia, render_ma
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches: ArgMatches = App::new("mandelbrot")
-                          .version("1.1.1")
+                          .version("1.2.1")
                           .author("Brent Mode <bmode@wisc.edu")
                           .about("creates mandelbrot and julia set images")
                           .subcommand(App::new("julia")
@@ -97,18 +97,34 @@ fn common_args<'a>(matches: &'a ArgMatches) -> (&'a str, (usize, usize), Complex
         .value_of("PIXELS")
         .unwrap(), 'x')
         .expect("error parsing image dimensions");
-    let ratio: f64 = bounds.0 as f64 / bounds.1 as f64; // x / y
-    let upper_left = parse_complex(matches
+    let mut ratio: f64 = bounds.0 as f64 / bounds.1 as f64; // x / y
+    let mut upper_left = parse_complex(matches
         .value_of("UPPERLEFT").unwrap())
         .expect("error parsing upper left corner point");
-    let lower_right = parse_complex(matches
+    let mut lower_right = parse_complex(matches
         .value_of("LOWERRIGHT").unwrap())
         .expect("error parsing lower right corner point");
     
     let plane_ratio = (lower_right.re - upper_left.re) / (upper_left.im - lower_right.im);
     if ratio != plane_ratio {
-       unimplemented!() 
+        if ratio > 1.0 {
+            ratio = 1.0 / ratio;
+            upper_left.im = upper_left.im - ratio / 2.0;
+            lower_right.im = lower_right.im + ratio / 2.0;
+        } else { 
+            upper_left.re = upper_left.re + ratio / 2.0;
+            lower_right.re = lower_right.re - ratio / 2.0;
+        }
+        println!("NEW UPPERLEFT\t\t{}", upper_left);
+        println!("NEW LOWERRIGHT\t   {}", lower_right);
     }
+    // 300x400 : (-2, 2), (1, -1)
+    // ratio = 300 / 400 = 0.75
+    // plane_ratio = 3 / 3 = 1.0
+    // cbound_y = 2 - (-1) = 3;
+    // cur_x = 1 - (-2) = 3;
+    // cbound_x = ratio * cbound_y = 0.75 * 3 = 2.25
+
     let colors: [color::Color; 2048];
     if let Some(color) = matches.value_of("COLORSCHEME") {
         colors = color::colors(color).unwrap();
